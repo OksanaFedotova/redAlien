126daa46f9720094b21111ac3c7558b36e709333
Oksana Fedotova <oxana-fedotova@yandex.ru>
2021-11-22 11:33:30 +0300

    screen size changed

--- src/main.js
+++ src/main.js
@@ -5,19 +5,14 @@ import GameOver from './scenes/GameOver';
 import bridge from '@vkontakte/vk-bridge';
 
 bridge.send('VKWebAppInit', {});
-bridge.send("VKWebAppShare", {"link": "https://vk.com/app6909581#hello"});
-bridge.send("VKWebAppAllowNotifications"); 
-
-// чтобы ловить события в консоль:
-bridge.subscribe((e) => {
-  console.log('bridge event', e);
-});
+//bridge.send("VKWebAppShare", {"link": "https://vk.com/app7995655_48927#hello"});
+//bridge.send("VKWebAppAllowNotifications"); 
 
 
 const config = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: 700,
+	height: 300,
 	physics: {
 		default: 'arcade',
 		arcade: {
