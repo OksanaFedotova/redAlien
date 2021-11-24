fc8ea6dfb34135f874c14bd8a52d80c102b51387
Oksana Fedotova <oxana-fedotova@yandex.ru>
2021-11-24 15:53:36 +0300

    size changed

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ bridge.send('VKWebAppInit', {});
 
 const config = {
 	type: Phaser.AUTO,
-	width: 1000,
-	height: 4050,
+	width: 800,
+	height: 600,
 	physics: {
 		default: 'arcade',
 		arcade: {
